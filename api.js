6d1d4c4c71f0b6ca454b4d850ea7f557423a3169
Lloyd Brookes <lloyd.brookes@gmail.com>
2013-03-23 19:36:58 +0000

    initial

---
+++ api.js
@@ -0,0 +1,10 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "HandbrakeProcess",
+        "handbrake-js"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
